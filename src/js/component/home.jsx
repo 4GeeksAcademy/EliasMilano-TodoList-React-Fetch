2dca98e27d40add81c5d54a4d2b904b78ed34ed6
Elías Milano <87205729+MilanoTester@users.noreply.github.com>
2024-10-22 00:02:17 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,21 +5,25 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	let tasksList = ['Red pills', 'Shower'];
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container mt-5 col-5">
+			<h1 className="text-center mt-5"> TODO LIST</h1>
+			<div className= "mt-3">
+				<input type="text" placeholder="Add a new task" className="mt-3 form-control bg-black text-success" id="exampleInputEmail1" aria-describedby="emailHelp" style={{fontFamily:"Roboto Mono", fontSize:"500"}}/>
+				<ul className="list-group mt-3">
+					<li className="d-flex justify-content-between bg-black border-bottom m-2">Make some red pills<i className="fa-solid fa-x hidden-icon"></i></li>
+					<li className="d-flex justify-content-between bg-black border-bottom m-2">Shower<i className="fa-solid fa-x hidden-icon"></i></li>
+					{tasksList.map= ((task,index) => {
+						return (
+							<li key={index} className="d-flex justify-content-between bg-black border-bottom m-2">{task}<i className="fa-solid fa-x hidden-icon"/></li>
+						)
+					})}
+				</ul>
+			</div>
 		</div>
+		
 	);
 };
 
