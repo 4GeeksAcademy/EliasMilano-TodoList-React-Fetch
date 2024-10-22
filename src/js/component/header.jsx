99f7a3fc7ebafec3cb35193eda466e7b39dd175d
Elías Milano <87205729+MilanoTester@users.noreply.github.com>
2024-10-22 19:27:42 +0000

    todo list finished

---
+++ src/js/component/header.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className="d-flex justify-content-between">
+            <div>
+                <div>
+                    Ship: Nebuchadnezzar
+                </div>
+                <div>
+                    Location: Unknown
+                </div>
+            </div>
+            <div>
+                <div>
+                    User: Morpheus
+                </div>
+                <div>
+                    Date: ??/??/2199
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import Header from "./header.jsx"
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	let tasksList = ['Red pills', 'Shower'];
+	const [tasksList, setTasksList] = useState(["Get a haircut", "Coffe with The Oracle", "Make some red pills", "Wake up Neo"]);
+	
+	const [newTask, setNewTask] = useState ("");
 
 	return (
 		<div className="container mt-5 col-5">
-			<h1 className="text-center mt-5"> TODO LIST</h1>
+			<Header/>
+			<h1 className="text-center mt-4">TODO LIST</h1>
 			<div className= "mt-3">
-				<input type="text" placeholder="Add a new task" className="mt-3 form-control bg-black text-success" id="exampleInputEmail1" aria-describedby="emailHelp" style={{fontFamily:"Roboto Mono", fontSize:"500"}}/>
-				<ul className="list-group mt-3">
-					<li className="d-flex justify-content-between bg-black border-bottom m-2">Make some red pills<i className="fa-solid fa-x hidden-icon"></i></li>
-					<li className="d-flex justify-content-between bg-black border-bottom m-2">Shower<i className="fa-solid fa-x hidden-icon"></i></li>
-					{tasksList.map= ((task,index) => {
-						return (
-							<li key={index} className="d-flex justify-content-between bg-black border-bottom m-2">{task}<i className="fa-solid fa-x hidden-icon"/></li>
-						)
-					})}
-				</ul>
+				<input type="text" placeholder="Add a new task" className="mt-3 form-control bg-black text-success" 
+					value={newTask} 
+					style={{fontFamily:"Roboto Mono", fontSize:"500"}} 
+					onChange={(event) => { 
+						setNewTask(event.target.value)
+					}}
+					onKeyUp={(event) => {
+						if (event.key == "Enter") {
+							setTasksList([...tasksList, newTask]);
+							setNewTask("");
+							// To observe the pressed keys on the console
+							console.log(event.key);
+						}
+					}}
+				/>
+				
+				<>
+					{(tasksList.length == 0) && <>
+						<div className="d-flex justify-content-center mt-4">
+							<h3 className="bg-secondary p-3 text-black rounded">THERE ARE NO TASKS</h3>
+						</div>
+					</>}
+						<ul className="list-group mt-3">
+							{tasksList.map((task,i) => {
+								return (
+									<li key={i} className="d-flex justify-content-between bg-black m-2">
+										{task} 
+										<i className="fa-solid fa-x hidden-icon" 
+											onClick={() => {
+												const filteredList = tasksList.filter((_item,j) => {
+													return (j != i);
+												})
+												setTasksList(filteredList);
+											}}
+										/>
+									</li>
+								)
+							})}
+						</ul>
+				</>
+				<div className="mt-3 py-1 px-3 border rounded">
+					<span style={{fontWeight:"400", fontSize: "normal"}}>{tasksList.length} items left</span>
+				</div>
 			</div>
 		</div>
 		
