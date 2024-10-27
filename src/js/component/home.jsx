9caa9cc678f1dd50ca0547ee6bcedea7f4e81aae
Elías Milano <87205729+MilanoTester@users.noreply.github.com>
2024-10-27 16:54:17 +0000

    project finished

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,10 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 import Header from "./header.jsx"
 
 const Home = () => {
 	const [tasksList, setTasksList] = useState(["Get a haircut", "Coffe with The Oracle", "Make some red pills", "Wake up Neo"]);
 	
 	const [newTask, setNewTask] = useState ("");
+
+	async function createUser() {
+		const url = "https://playground.4geeks.com/todo/users/morpheus";
+		const response = await fetch(url, {
+			method: "POST"
+		})
+		if (response.ok){
+			getTasks();
+		}		
+	}
+
+	const getTasks = async() => {
+		const url = "https://playground.4geeks.com/todo/users/morpheus";
+		const response = await fetch(url);
+		if (response.status == 404) {
+			createUser();
+			return;
+		}
+		const data = await response.json();
+		setTasksList(data.todos);
+	}
+
+	useEffect(() => {
+		getTasks();
+	}, [])
+
 
 	return (
 		<div className="container mt-5 col-5">
@@ -17,16 +43,27 @@ const Home = () => {
 					onChange={(event) => { 
 						setNewTask(event.target.value)
 					}}
-					onKeyUp={(event) => {
+					onKeyUp={async(event) => {
 						if (event.key == "Enter") {
-							setTasksList([...tasksList, newTask]);
-							setNewTask("");
-							// To observe the pressed keys on the console
-							console.log(event.key);
+							const url = "https://playground.4geeks.com/todo/todos/morpheus";
+							const response = await fetch(url,{
+								method: "POST",
+								headers: {
+									"Content-Type": "application/json",
+								  },
+								body: JSON.stringify({
+									label: newTask,
+									is_done: false
+								})
+							})
+							if (response.status == 201) {
+								getTasks();
+								setNewTask("");
+							}
 						}
 					}}
 				/>
-				
+
 				<>
 					{(tasksList.length == 0) && <>
 						<div className="d-flex justify-content-center mt-4">
@@ -34,16 +71,22 @@ const Home = () => {
 						</div>
 					</>}
 						<ul className="list-group mt-3">
-							{tasksList.map((task,i) => {
+							{tasksList && tasksList.map((task,i) => {
 								return (
 									<li key={i} className="d-flex justify-content-between bg-black m-2">
-										{task} 
+										{task.label} 
 										<i className="fa-solid fa-x hidden-icon" 
-											onClick={() => {
+											onClick={async() => {
+												const url = "https://playground.4geeks.com/todo/todos/" + task.id;
 												const filteredList = tasksList.filter((_item,j) => {
 													return (j != i);
 												})
-												setTasksList(filteredList);
+												const response = await fetch(url, {
+													method: "DELETE"
+												})
+												if (response.ok) {
+													setTasksList(filteredList);
+												}
 											}}
 										/>
 									</li>
@@ -52,7 +95,7 @@ const Home = () => {
 						</ul>
 				</>
 				<div className="mt-3 py-2 px-3 border-top">
-					<span style={{fontWeight:"400", fontSize: "normal"}}>{tasksList.length} items left</span>
+					<span style={{fontWeight:"400", fontSize: "normal"}}>{tasksList && tasksList.length} items left</span>
 				</div>
 			</div>
 		</div>
